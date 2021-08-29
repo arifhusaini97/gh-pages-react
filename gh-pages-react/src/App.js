1a362af6f35dc2f18f449aafc73a04ca8acb8cb4
NUR ARIF HUSAINI BIN NORWAZA <chemist.arif.husaini@gmail.com>
2021-08-29 10:08:06 +0800

    Updated homepage

--- gh-pages-react/src/App.js
+++ gh-pages-react/src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to <code>arifhusaini97.github.io</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          React Web Development
       </header>
     </div>
   );
